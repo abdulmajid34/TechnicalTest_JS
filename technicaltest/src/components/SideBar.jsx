6e1c84aa6a9c930083ac34d3dbc408b84ea32ce7
abdulmajid <abdulmajid08180881@gmail.com>
2021-06-17 22:36:44 +0700

    added sideBar

--- technicaltest/src/components/SideBar.jsx
+++ technicaltest/src/components/SideBar.jsx
@@ -1,34 +1,10 @@
-import React, { useState, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-// import SideBar from '../components/SideBar'
-import { useFetchApi } from '../hooks/useFetchApi' 
-import { useHistory } from 'react-router-dom'
-import Loading from './Loading'
-import NotFound from './NotFound'
+import React from 'react'
 
 
-function Dashboard() {
-    // const dispatch = useDispatch()
-    const [dataCovid, loading, error] = useFetchApi('https://covid-19-tracking.p.rapidapi.com/v1')
-    console.log(dataCovid, 'INI DATA COVID NYA');
-
-    const history = useHistory()
-    
-    const handleLogout = () => {
-        localStorage.clear()
-        history.push('/')
-    }
-
-    if(loading) {
-        return <Loading />
-    }
-    if(error) {
-        return <NotFound />
-    }
+function SideBar() {
 
     return (
-        <div className="h-screen w-screen bg-gray-200 flex flex-row">
-            <div className="h-screen bg-gray-200 flex w-screen flex-row">
+        <div className="h-screen bg-gray-200 flex w-screen flex-row">
             <nav className="h-full w-80 bg-gray-200" style={{ width: '20' }}>
                 <div className="w-full h-1/6">
                     <div className="h-screen bg-yellow-500">
@@ -49,19 +25,15 @@ function Dashboard() {
                                     <p className="text-2xl cursor-pointer hover:text-gray-900 focus:text-gray-900 transition hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline rounded-full text-white">Contact</p>
                                 </div>
                                 <div className="flex flex-col pt-10">
-                                    <p onClick={() => handleLogout()} className="text-2xl hover:text-gray-900 focus:text-gray-900 transition hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline rounded-full text-white cursor-pointer">Logout</p>
+                                    <p className="text-2xl hover:text-gray-900 focus:text-gray-900 transition hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline rounded-full text-white cursor-pointer">Logout</p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </nav>
-            <div className="">
-                <h1>hello world</h1>
-            </div>
-            </div>
         </div>
     )
 }
 
-export default Dashboard
+export default SideBar
